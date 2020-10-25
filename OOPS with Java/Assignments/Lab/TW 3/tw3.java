d5896c2712803bf8e0ce48887ed09f83252ce204
Venkatesh D <venkigd8@gmail.com>
2020-10-25 13:10:51 +0530

    Added TW 3 and 4

---
+++ OOPS with Java/Assignments/Lab/TW 3/tw3.java
@@ -0,0 +1,34 @@
+package tw3;
+import java.util.Scanner;
+
+class Rectangle{
+	int length,breadth;
+	Rectangle(int l,int b){
+		length=l;
+		breadth=b;
+	}
+	Rectangle(){
+		//initialize an object
+	}
+	void changeDim(int length,int breadth) {
+		this.length=length;
+		this.breadth=breadth;
+	}
+	int area() {
+		return length*breadth;
+	}
+}
+public class tw3 {
+
+	public static void main(String[] args) {
+		Rectangle r1=new Rectangle(20,40);
+		System.out.println("Area of rectangle for object r1 :"+r1.area());
+		Scanner s=new Scanner(System.in);
+		Rectangle r2=new Rectangle();
+		System.out.println("Enter the length and breadth for rectangle: ");
+		int length=s.nextInt();
+		int breadth=s.nextInt();
+		r2.changeDim(length, breadth);
+		System.out.println("Area of rectangle for object r2 :"+r2.area());
+	}
+}

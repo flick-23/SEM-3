0d0e41a05f4418eb1b0fe1751f068235f7a01d12
flick-23 <venkigd8@gmail.com>
2020-10-28 20:11:24 +0530

    Adding codes for structures

---
+++ Data Structures/Codes/Structures.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+//declaration of a Structure
+
+
+int main()
+{
+    /*Structure is a collection of one or more variables of same data type 
+        or dissimilar datatype grouped under a single name
+    */
+
+}

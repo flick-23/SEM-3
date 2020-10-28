1d3cdfec7fe07a0c261e8e50dcab36c4085f1260
flick-23 <venkigd8@gmail.com>
2020-10-28 20:02:10 +0530

    Completed codes for pointers

--- Data Structures/Codes/Pointers.c
+++ Data Structures/Codes/Pointers.c
@@ -35,6 +35,14 @@ int main()
 
     int **p4;   //Declaration of Pointer to Pointer variable
     int d = 40;
+
+    int *p5;
+    int array[5] ={10,20,30,40,50};
+
+    int *x;     // x is going to be an array. Learn more in  Array names as pointer section
+    
+    int y[2][2]={{10,20},{30,40}}   // pointers in MULTIDIMENSIONAL arrays 
+
     printf("\nThe address of a : %p",p1);       // %p is used to display pointers (instead of % d)
     printf("\nThe address of b : %p",&b);       // Direct approach of printing the address
     printf("\nThe address of c : %p",p3);      
@@ -78,6 +86,45 @@ int main()
     printf("\nPOINTERS TO POINTERS");
     printf("\nAddress stored at p4 (copying address of p1) : %p",p4);   
     printf("\nValue at address stored in P4 : %d",**p4);    //this prints the value at address of p1
-    printf("\nValue of P1 : %d",*p1);
+    printf("\nValue of P1 : %d\n\n",*p1);
+
+    // ===================================Pointer Expressions and Pointer Arithmetic ==================================
+    /*
+    Pointer arithmetic is meaningless unless performed on an array.
+    Note : Pointers contain addresses. Adding two addresses makes no sense, because there is no idea what it would point to. 
+    Subtracting two addresses lets you compute the offset between these two addresses.
+    */
+   p5 = array;  //p5 now has the address of array[0]
+   for(int i = 0; i<5; i++)
+   {
+       printf("\nValue at array[%d] : %d",i,*(p5+i));
+       printf("\nAddress of array[%d] : %p",i,(p5+i));
+
+       // if not => *(p5+1) alternatively use
+       // p5++  
+       // the prior is preffered in order to NOT loose the location of array index at 0
+   }
+   // ======================================= ARRAY NAME AS POINTERS =============================================================
+
+    x = array; // alternatively => x = &array[0];
+    printf("\n\nElements of X : ");
+    printf("\nX[0] : %d",x[0]);
+    printf("\nX[1] : %d",x[1]);
+    printf("\nX[2] : %d",x[2]);
+    printf("\nX[3] : %d",x[3]);
+    printf("\nX[4] : %d",x[4]);
+//============================================ POINTERS IN MULTIDIMENSIONAL ARRAY ============================================
+
+    /**
+       POINTER NOTATION     	ARRAY NOTATION	VALUE
+                *(*nums)	    nums[0][0]	    16
+            *(*nums + 1)	    nums[0][1]	    18
+            *(*nums + 2)	    nums[0][2]	    20
+            *(*(nums + 1))	    nums[1][0]	    25
+            *(*(nums + 1) + 1)	nums[1][1]	    26
+            *(*(nums + 1) + 2)	nums[1][2]	    27
+     */
+    
+
 }
 

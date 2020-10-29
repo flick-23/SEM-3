b4c671900a12c1ba76b793071439a12a492b155f
Venkatesh D <venkigd8@gmail.com>
2020-10-29 22:57:03 +0530

    Added Diagram and Problem statement for Java project

--- Data Structures/Codes/Structures.c
+++ Data Structures/Codes/Structures.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 
-//declaration of a Structure
+//declaration of a Tagged structure
+struct address 
+{ 
+   char name[50]; 
+   char street[100]; 
+   char city[50]; 
+   char state[20]; 
+   int pin; 
+};     // alternatively =>  }obj;
 
 
 int main()
@@ -8,5 +16,6 @@ int main()
     /*Structure is a collection of one or more variables of same data type 
         or dissimilar datatype grouped under a single name
     */
-
+   struct address ad;
+   ad.name = "flick23";
 }

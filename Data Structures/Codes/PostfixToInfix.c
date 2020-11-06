ac2665bc5b5a93a8fc5f900cfb7b1d4521399be9
flick-23 <venkigd8@gmail.com>
2020-11-06 21:09:23 +0530

    Documented and Optimized

--- Data Structures/Codes/PostfixToInfix.c
+++ Data Structures/Codes/PostfixToInfix.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<math.h>
 #include<string.h>
 struct Stack
 {
@@ -7,17 +8,18 @@ struct Stack
     int *a;
     int top;
 };
-int isEmpty(struct Stack *s)
+
+int isEmpty(struct Stack *s)    //checks if a stack is empty
 {
     return s->top == s->capacity;
 }
-void push(struct Stack *s,int op)
+void push(struct Stack *s,int op)   //add an item to the top of the  stack
 {
     s->a[++s->top] = op;
     printf("\nAdded in stack : %d",s->a[s->top]);
 }
 
-int pop(struct Stack *s)
+int pop(struct Stack *s)    // remove an item from the top of the stack 
 {
     if(!isEmpty(s))
     {
@@ -25,7 +27,7 @@ int pop(struct Stack *s)
     }
 }
 
-int peek(struct Stack *s)
+int peek(struct Stack *s)   // prints the top item in the stack
 {
     if(!isEmpty(s))
     {
@@ -33,7 +35,7 @@ int peek(struct Stack *s)
     }
 }
 
-void postFixToInFix(char *exp)
+void postFixToInFix(char *exp)      //converts postfix to infix 
 {
     int i,op1=0,op2=0,result=0,x=0;
     struct Stack *s = (struct Stack*)malloc(sizeof(struct Stack));
@@ -43,12 +45,13 @@ void postFixToInFix(char *exp)
     
     for(i=0; i<s->capacity;i++)
     {
-        if(exp[i]>='0' && exp[i]<='9')
+        // if it is a number, add it to a stack
+        if(exp[i]>='0' && exp[i]<='9')  
         {
             x = exp[i]-'0';
-            printf("\nX : %d",x);
             push(s,x);
         }
+        // if it is an operand, then pop two items and perform the particular operation
         else 
         {
             op2 = pop(s);
@@ -58,16 +61,20 @@ void postFixToInFix(char *exp)
             switch(exp[i])
             {
                 case '+':result=op2+op1;
+                printf("\nAdded both : %d",result);
                 break;
                 case '-':result=op1-op2;
+                printf("\nSubtracted both : %d",result);
                 break;
                 case '*':result=op1*op2;
+                printf("\nMultiplication both : %d",result);
                 break;
                 case '/':result= op1/op2;
+                printf("\nDivided both : %d",result);
                 break;
                 case'^':
                 case'$':
-                result = op1^op2;
+                result = pow(op1,op2);
                 break;
                 default:
                 printf("\nIncorrect operator!");

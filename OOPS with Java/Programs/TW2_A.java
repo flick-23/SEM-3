ccbff80e96972fada672bc4ee266b4e4db173a52
flick-23 <venkigd8@gmail.com>
2021-03-21 00:01:11 +0530

    Added solution for TW 2 problem A

--- OOPS with Java/Programs/TW2_A.java
+++ OOPS with Java/Programs/TW2_A.java
@@ -13,28 +13,7 @@ class Rectangle{
         return area;
     }
 }
-class Triangle{
-    double a,b,c;
-    void getSides(double x,double y,double z){
-        a=x;
-        b=y;
-        c=z;
-    }
-    void checkType(){
-        if(a==b && b==c)
-            System.out.println("Equilateral Triangle!");
-        else if((a==b && a!=c) || (b==c && b!=a) || (c==a && a!=b))
-            System.out.println("Isoceles Triangle!");
-        else
-            System.out.println("Scalene Triangle!");
-    }
-    double computeArea(){
-        double s = (a+b+c)/2;
-        s = s*(s-a)*(s-b)*(s-c);
-        s = Math.sqrt(s);
-        return s;
-    }
-}
+
 
 public class TW2_A{
     public static void main(String[]args){
@@ -46,14 +25,5 @@ public class TW2_A{
         int b= venki.nextInt();
         rt.setDim(l,b);
         System.out.println("Area of rectangle : "+rt.getArea());
-
-        Triangle tr = new Triangle();
-        System.out.println("Enter the 3 sides of triangle - ");
-        double A = venki.nextDouble();
-        double B = venki.nextDouble();
-        double C = venki.nextDouble();
-        tr.getSides(A,B,C);
-        tr.checkType();
-        System.out.println("Area of triangle : "+tr.computeArea());
     }
 }
